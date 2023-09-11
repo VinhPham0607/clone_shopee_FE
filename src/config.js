247329a84e7d297eebe6f05704b4f981ae2ebb6f
VinhPham0607 <20021477@vnu.edu.vn>
2023-09-11 22:37:30 +0700

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: 'https://shopeeclonebe-production.up.railway.app/api/v1'
+    baseUrl: 'https://cloneshopeebe-production.up.railway.app/api/v1'
 }
 
 export default config;
